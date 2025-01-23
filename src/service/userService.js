5b414407003e3d10a0c600ca18629506497e3edf
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-01-23 12:28:41 -0800

    updated

--- src/service/userService.js
+++ src/service/userService.js
@@ -6,58 +6,54 @@ import UserModel from "../model/userModel.js";
 
 class UserService {
   static async login(res, { email, password }) {
-    try {
-      if (!email && !password)
-        return {
-          statusCode: 422,
-          message: "Provide email and password",
-        };
-
-      const getUser = await UserModel.findOne({ email });
-
-      if (!getUser)
-        return {
-          statusCode: 404,
-          message: "Invalid email/password",
-        };
-
-      const comparePassword = HelperFunction.comparePassword(
-        getUser.password,
-        password
-      );
-
-      if (!comparePassword)
-        return {
-          statusCode: 404,
-          message: "Invalid email/password",
-        };
-
-      const accessToken = JwtHelper.generateAccessToken(getUser);
-
-      const refreshToken = JwtHelper.generateRefreshToken(getUser);
-
-      res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "strict",
-        maxAge: 15 * 60 * 1000,
-      });
-
-      res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-      });
-
+    if (!email && !password)
       return {
-        statusCode: 200,
-        message: "log in successful",
-        data: { user: getUser.name, refreshToken, accessToken },
+        statusCode: 422,
+        message: "Provide email and password",
       };
-    } catch (err) {
-      Logger.error("loginService Error:", err);
-    }
+
+    const getUser = await UserModel.findOne({ email });
+
+    if (!getUser)
+      return {
+        statusCode: 404,
+        message: "Invalid email/password",
+      };
+
+    const comparePassword = HelperFunction.comparePassword(
+      getUser.password,
+      password
+    );
+
+    if (!comparePassword)
+      return {
+        statusCode: 404,
+        message: "Invalid email/password",
+      };
+
+    const accessToken = JwtHelper.generateAccessToken(getUser);
+
+    const refreshToken = JwtHelper.generateRefreshToken(getUser);
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 15 * 60 * 1000,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return {
+      statusCode: 200,
+      message: "log in successful",
+      data: { user: getUser.name, refreshToken, accessToken },
+    };
   }
 }
 
