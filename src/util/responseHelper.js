4c00bedb371ac103fa1d64020056df16d45403b4
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-01-24 00:43:25 -0800

    updated

--- src/util/responseHelper.js
+++ src/util/responseHelper.js
@@ -1,10 +1,10 @@
 class ResponseHelper {
   static successResponse(res, statuscode, message, data) {
-    return res.status(statuscode).json({ status: "success" }, message, data);
+    return res.status(statuscode).json({ status: "success", message, data });
   }
 
   static errorResponse(res, statuscode, message, data) {
-    return res.status(statuscode).json({ status: "failed" }, message, data);
+    return res.status(statuscode).json({ status: "failed", message, data });
   }
 }
 
