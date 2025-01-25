fd230ae2b0a6af7a9c980f40286692c5a2584336
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-01-25 05:21:32 -0800

    updated login limiter

--- src/config/rateLimit.js
+++ src/config/rateLimit.js
@@ -7,8 +7,16 @@ export const apiLimiter = rateLimiter({
   headers: true,
 });
 
+// Define rate-limiting rules
 export const loginLimiter = rateLimiter({
-  windowMs: 10 * 60 * 1000,
-  max: 5,
-  message: "too many login attempt! Please try again later",
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 5 login attempts per `windowMs`
+  message: {
+    message:
+      "Too many login attempts from this IP, please try again after 15 minutes.",
+  },
+  standardHeaders: true, // Return rate limit info in `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
+
+// Apply rate-limiting middleware to the login route
