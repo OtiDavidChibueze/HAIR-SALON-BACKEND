d3042dbf4f105d616f465a4f5f4cfd7d974516de
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-01-26 03:28:22 -0800

    added reuseable token cookies functions

--- src/util/helperFunction.js
+++ src/util/helperFunction.js
@@ -2,7 +2,6 @@ import bcrypt from "bcrypt";
 import ResponseHelper from "./responseHelper.js";
 import mongoose from "mongoose";
 import { Logger } from "../config/logger.js";
-import redisClient from "../config/redis.js";
 
 class HelperFunction {
   static async hashPassword(newPassword) {
@@ -22,6 +21,62 @@ class HelperFunction {
 
   static async comparePassword(newPassword, oldPassword) {
     return await bcrypt.compare(newPassword, oldPassword);
+  }
+
+  static accessTokenCookie(
+    type = "accessToken",
+    token,
+    object = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 15 * 60 * 1000,
+    }
+  ) {
+    return (req, res, next) => {
+      res.cookie(type, token, object);
+
+      console.log({ resAccess: res });
+
+      next();
+    };
+  }
+
+  static refreshTokenCookie(
+    type = "refreshToken",
+    token,
+    object = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    }
+  ) {
+    return (req, res, next) => {
+      res.cookie(type, token, object);
+
+      console.log({ resFresh: res });
+
+      next();
+    };
+  }
+
+  static accessTokenCookie(res, token) {
+    return res.cookie("accessToken", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 15 * 60 * 1000,
+    });
+  }
+
+  static refreshTokenCookie(res, token) {
+    return res.cookie("refreshToken", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
   }
 }
 
