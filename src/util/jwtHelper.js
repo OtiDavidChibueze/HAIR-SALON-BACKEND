9a395634ebb6a959359f30feb87cb3776ef66cbc
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-01-29 03:43:25 -0800

    added decode verification token

--- src/util/jwtHelper.js
+++ src/util/jwtHelper.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import {
   PRODUCTION_REFRESH_SECRET_KEY,
   PRODUCTION_SECRET_KEY,
+  PRODUCTION_VERIFICATION_SECRET,
 } from "../config/keys.js";
 import { Logger } from "../config/logger.js";
 
@@ -22,6 +23,14 @@ class JwtHelper {
     }
   }
 
+  static decodeVerificationToken(VerificationToken) {
+    try {
+      return jwt.verify(VerificationToken, PRODUCTION_VERIFICATION_SECRET);
+    } catch (err) {
+      return Logger.error("decodeVerificationToken", err);
+    }
+  }
+
   static generateAccessToken(user) {
     return jwt.sign({ id: user.id, role: user.role }, PRODUCTION_SECRET_KEY, {
       expiresIn: "15min",
@@ -39,9 +48,13 @@ class JwtHelper {
   }
 
   static generateVerificationToken(user) {
-    return jwt.sign({ id: user.id, role: user.role }, PRODUCTION_SECRET_KEY, {
-      expiresIn: "1h",
-    });
+    return jwt.sign(
+      { id: user.id, role: user.role },
+      PRODUCTION_VERIFICATION_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
   }
 }
 
