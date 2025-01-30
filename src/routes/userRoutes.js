80d435ea1c8783fab2cc69a199250f657072279e
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-01-30 08:46:37 -0800

    updated createUser to signIn and added deleteAccountById endpoint

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -22,7 +22,7 @@ route.post(
 route.post(
   "/createUser",
   SchemaValidationHelper.validateInput(createUser),
-  UserController.createUser
+  UserController.signIn
 );
 route.post(
   "/upload-profile-pic",
@@ -50,57 +50,17 @@ route.get("/refresh", JwtAuth, UserController.refreshToken);
 
 route.delete("/delete-profile-pic", JwtAuth, UserController.deleteProfilePic);
 route.delete("/delete-your-account", JwtAuth, UserController.deleteYourAccount);
-// delete profile pic by id
 route.delete(
   "/delete-profile-pic/:id",
   JwtAuth,
   RBAC_Auth("Admin", "SuperAdmin"),
   UserController.deleteProfilePicByUserId
 );
-
-// delete user by id
-// update profile
-// update profile by id
-// get all users
-// get all admins
-// get all superAdmins
-// get total user total count
-// get total admin total count
-// get total superAdmin total count
-// find closest admin(hairStylist)
-// static async findNearby(req, res) {
-//   const { longitude, latitude, maxDistance = 5000 } = req.query; // Default max distance: 5km
-
-//   if (!longitude || !latitude) {
-//     return res.status(400).json({
-//       status: "error",
-//       message: "Longitude and latitude are required",
-//     });
-//   }
-
-//   try {
-// const hairstylists = await UserModel.find({
-//   role: "hairstylist",
-//   location: {
-//     $near: {
-//       $geometry: { type: "Point", coordinates: [longitude, latitude] },
-//       $maxDistance: maxDistance,
-//     },
-//   },
-// }).sort({ location: 1 }); // Sort by proximity
-
-//     res.status(200).json({
-//       status: "success",
-//       message: "Nearby hairstylists found",
-//       data: hairstylists,
-//     });
-//   } catch (err) {
-//     res.status(500).json({
-//       status: "error",
-//       message: "Internal server error",
-//       error: err.message,
-//     });
-//   }
-// }
+route.delete(
+  "/delete-account/:id",
+  JwtAuth,
+  RBAC_Auth("Admin", "SuperAdmin"),
+  UserController.deleteAccountById
+);
 
 export default route;
