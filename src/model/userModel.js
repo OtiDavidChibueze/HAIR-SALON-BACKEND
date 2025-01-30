d180451704eb8e3f9bdabd4de796a22376b4f94f
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-01-30 04:51:50 -0800

    updated

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,42 +1,43 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    phone: { type: String, unique: true, required: true },
+    role: {
+      type: String,
+      enum: ["User", "Admin", "SuperAdmin"],
+      default: "User",
+    },
+    address: {
+      street: { type: String },
+      city: { type: String },
+      state: { type: String },
+      postalCode: { type: String },
+      country: { type: String },
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point",
+      },
+      coordinates: {
+        type: [Number], // [longitude, latitude]
+        index: "2dsphere",
+      },
+    },
     profilePic: {
       url: { type: String, default: "" },
       publicId: { type: String, default: "" },
     },
-
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-
-    password: {
-      type: String,
-      required: true,
-    },
-
-    role: {
-      type: String,
-      enum: ["customer", "admin", "superAdmin"],
-      default: "customer",
-    },
-
-    createdAt: { type: Date, default: Date.now() },
-
+    isActive: { type: Boolean, default: true },
     isVerified: { type: Boolean, default: false },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-const UserModel = mongoose.model("user", userSchema);
-
+const UserModel = mongoose.model("User", userSchema);
 export default UserModel;
